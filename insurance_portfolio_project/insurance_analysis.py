772693753679fd39b993456dddc06c11a3466a86
Myrisa Mitchell <myrisamitchell@gmail.com>
2022-07-28 10:59:12 -0600

    added average age function

--- insurance_portfolio_project/insurance_analysis.py
+++ insurance_portfolio_project/insurance_analysis.py
@@ -21,14 +21,23 @@ def create_dicts(lst, csv_file, column_name):
         for row in csv_dict:
             lst.append(row[column_name])
         return lst
+
+def ave_age(lst):
+    pass
+    total = 0
+    for i in lst:
+        total += int(i)
+    return str(int((total/len(lst))))
     
 def main():
-    create_dicts(ages, 'insurance.csv', 'age')
-    create_dicts(genders, 'insurance.csv', 'sex')
-    create_dicts(bmis, 'insurance.csv', 'bmi')
-    create_dicts(children, 'insurance.csv', 'children')
-    create_dicts(smoker_stat, 'insurance.csv', 'smoker')
-    create_dicts(region, 'insurance.csv', 'region')
-    create_dicts(ins_charges, 'insurance.csv', 'charges')
+    create_dicts(ages, 'insurance_portfolio_project/insurance.csv', 'age')
+    create_dicts(genders, 'insurance_portfolio_project/insurance.csv', 'sex')
+    create_dicts(bmis, 'insurance_portfolio_project/insurance.csv', 'bmi')
+    create_dicts(children, 'insurance_portfolio_project/insurance.csv', 'children')
+    create_dicts(smoker_stat, 'insurance_portfolio_project/insurance.csv', 'smoker')
+    create_dicts(region, 'insurance_portfolio_project/insurance.csv', 'region')
+    create_dicts(ins_charges, 'insurance_portfolio_project/insurance.csv', 'charges')
+
+    print("The average age is " + ave_age(ages) + ".")
 
 main()
